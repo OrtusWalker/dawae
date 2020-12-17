ff1f43d2fc1651e487b5af3dc048129a39cdffed
Ortus <dev.type@inbox.ru>
2020-12-17 23:33:42 +0800

    design fix 1

--- DaWae/DaWae/Form1.Designer.cs
+++ DaWae/DaWae/Form1.Designer.cs
@@ -61,7 +61,6 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(128, 64);
             this.label1.TabIndex = 3;
-            this.label1.Text = "text1231313";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button2
