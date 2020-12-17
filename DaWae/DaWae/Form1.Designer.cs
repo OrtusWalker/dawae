914c72416d0e072ca6377981ddf245854448aed8
Ortus <dev.type@inbox.ru>
2020-12-18 02:22:17 +0800

    Bug Fix

--- DaWae/DaWae/Form1.Designer.cs
+++ DaWae/DaWae/Form1.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.authPanel = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.authPanel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,31 +50,50 @@
             this.authPanel.Controls.Add(this.button1);
             this.authPanel.Controls.Add(this.textBox2);
             this.authPanel.Controls.Add(this.textBox1);
-            this.authPanel.Location = new System.Drawing.Point(533, 171);
-            this.authPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.authPanel.Location = new System.Drawing.Point(400, 139);
             this.authPanel.Name = "authPanel";
-            this.authPanel.Size = new System.Drawing.Size(285, 327);
+            this.authPanel.Size = new System.Drawing.Size(214, 266);
             this.authPanel.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label3.Location = new System.Drawing.Point(44, 113);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label2.Location = new System.Drawing.Point(44, 73);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Login:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label1.Location = new System.Drawing.Point(59, 235);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.MinimumSize = new System.Drawing.Size(171, 79);
+            this.label1.Location = new System.Drawing.Point(44, 191);
+            this.label1.MinimumSize = new System.Drawing.Size(128, 64);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(171, 79);
+            this.label1.Size = new System.Drawing.Size(128, 64);
             this.label1.TabIndex = 3;
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(157, 203);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(118, 165);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(70, 28);
+            this.button2.Size = new System.Drawing.Size(52, 23);
             this.button2.TabIndex = 2;
             this.button2.Text = "Signup";
             this.button2.UseVisualStyleBackColor = true;
@@ -82,10 +101,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(60, 203);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(45, 165);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(69, 28);
+            this.button1.Size = new System.Drawing.Size(52, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Signin";
             this.button1.UseVisualStyleBackColor = true;
@@ -93,51 +111,28 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(62, 160);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Location = new System.Drawing.Point(46, 130);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(165, 22);
+            this.textBox2.Size = new System.Drawing.Size(125, 20);
             this.textBox2.TabIndex = 1;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(62, 111);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(46, 90);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(165, 22);
+            this.textBox1.Size = new System.Drawing.Size(125, 20);
             this.textBox1.TabIndex = 0;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.label2.Location = new System.Drawing.Point(59, 90);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 17);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Login:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.label3.Location = new System.Drawing.Point(59, 139);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(73, 17);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Password:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.BackgroundImage = global::DaWae.Properties.Resources.background_mainmenu;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1312, 678);
+            this.ClientSize = new System.Drawing.Size(984, 551);
             this.Controls.Add(this.authPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "DaWae";
             this.Load += new System.EventHandler(this.MainForm_Load);
